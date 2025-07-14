aaf0b1debfc625ea9dc8d9ca9cbae6d32dc58192
raoshah <shahrukhrao1@gmail.com>
2025-07-14 18:05:14 +0530

    wallet setup using tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Wallet from './Wallet';
 
-
-function App() {
-  return "hello";
-}
+const App = () => {
+  return (
+    <div className="flex justify-center items-start bg-gray-100 p-2">
+      <div className="w-[360px] h-[650px] overflow-y-auto">
+        <h1 className="text-xl font-bold mb-4 text-center">SyncRao Wallet</h1>
+        <Wallet />
+      </div>
+    </div>
+  )
+};
 
 export default App;
